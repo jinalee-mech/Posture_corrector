88bf940aa5c920f207d63c57f2986e7a5e93b932
jinalee-mech <90688988+jinalee-mech@users.noreply.github.com>
2022-11-22 20:12:13 +0900

    Update video.js

--- video.js
+++ video.js
@@ -4,10 +4,10 @@ let poses = [];
 let started = false;
 let LeftEarPosition = [];
 let RightEarPosition = [];
-let Leftshoulderosition = [];
-let RightshoulderPosition = [];
 let LeftEyePosition = [];
 let RightEyePosition = [];
+let angles = [];
+let angle;
 //p5.js
 
 function setup() {
@@ -35,36 +35,17 @@ function draw() {
     if (started) {
         image(videofeed, 0, 0, width, height);
         calEyes();
+        
 
     }
 }
-function wri(){
+function wri(){ //
     // creates a file called 'newFile.txt'
   let writer = createWriter('허리구부정.txt');
   // write 'Hello world!'' to the file
-  writer.write(['defaultRightEarPosition:']);
-  writer.write([500-defaultRightEarPosition]); //start 눌렀을때
-  writer.write(['\n']);
-  writer.write(['RightEarPosition:']);
-  writer.write([500-RightEarPosition]);
-  writer.write(['\n']);
-  writer.write(['defaultRightShoulderPosition:']);
-  writer.write([500-defaultRightshoulderPosition]);
-  writer.write(['\n']);
-  writer.write(['RightshoulderPosition']);
-  writer.write([500-RightshoulderPosition]);
-  writer.write(['\n']);
-  writer.write(['defaultRightEyePosition:']);
-  writer.write([500-defaultRightEyePosition]);
-  writer.write(['\n']);
-  writer.write(['RightEyePosition']);
-  writer.write([500-RightEyePosition]);
-  writer.write(['\n']);
-  writer.write(['defaultear - defaultshoulder: ']);
-  writer.write([defaultRightshoulderPosition-defaultRightEarPosition]);
-  writer.write(['\n']);
-  writer.write(['ear - shoulder: ']);
-  writer.write([RightshoulderPosition-RightEarPosition]);
+  writer.write(['angle:']);
+  writer.write([angles]);
+
   // close the PrintWriter and save the file
   writer.close();
   
@@ -85,17 +66,20 @@ function stop() {
   
     noLoop();
 }
+function calcAngle(a, b){
+        angleMode(DEGREES);
+        if (a.x == b.x)
+            return 1.570796
+        return atan2(b.y-a.y, b.x-a.x)
+}
 
 var rightEye,
     leftEye,
     leftear,
     rightear,
-    leftshoulder,
-    rightshoulder,
+    lefthip
     defaultRightEarPosition = [],
     defaultLeftEarPosition = [],
-    defaultRightshoulderPosition = [],
-    defaultLeftshoulderPosition = [],
     defaultRightEyePosition = [],
     defaultLeftEyePosition = [];
 
@@ -106,50 +90,34 @@ function calEyes() {
         for (let j = 0; j < pose.keypoints.length; j++) {
             let keypoint = pose.keypoints[j];
             rightear = pose.keypoints[4].position;
+            leftear = pose.keypoints[3].position;
             rightshoulder = pose.keypoints[6].position;
             rightEye = pose.keypoints[2].position;
-
-
-            while (defaultRightEyePosition.length < 1) {
+            lefthip = pose.keypoints[11].position;
+          while (defaultRightEyePosition.length < 1) { //start를 눌렀을때의 기준 눈의 높이
                 defaultRightEyePosition.push(rightEye.y);
             }
-
-            while (defaultRightEarPosition.length < 1) {
-                defaultRightEarPosition.push(rightear.y);
-            }
-
-            while (defaultRightshoulderPosition.length < 1) {
-                defaultRightshoulderPosition.push(rightshoulder.y);
-            }
-          
-
-
-
-
-
-
-
-            if (Math.abs(rightEye.y - defaultRightEyePosition[0]) > 20) {
-                blur();
-              
-            while (RightEarPosition.length < 1) {
-                RightEarPosition.push(rightear.y);
-            }
-              while (RightshoulderPosition.length < 1) {
-                RightshoulderPosition.push(rightshoulder.y);
-            }
-              
-              while (RightEyePosition.length < 1) {
-                RightEyePosition.push(rightEye.y);
-            }
-              
-
-            }
-            if (Math.abs(rightEye.y - defaultRightEyePosition[0]) <= 20) {
-                removeblur();
-            }
-        }
+            angle = calcAngle(leftear, lefthip); //귀와 엉덩이의 각도측정
+          stroke('red')
+         strokeWeight(10);
+        point(leftear.x, leftear.y);
+         point(lefthip.x, lefthip.y);
+          noStroke();
+          text('Angle: '+ angle, 50, 50, 100, 100)
+          //눈 detection
+          if (Math.abs(rightEye.y - defaultRightEyePosition[0]) > 20) {
+                blur();}
+            
+          //등 detection
+          if(angle < 83) //굽은등일때
+          {blur();
+           angles.push(angle); //굽은등일때의 각도를 txt저장
+          }
+          else
+            removeblur();
+        
     }
+}
 }
 
 
@@ -164,91 +132,4 @@ function blur() {
 function removeblur() {
     document.body.style.filter = "blur(0px)";
     // var audio = document.getElementById("audioElement").pause();
-}
-
-function start() {
-    select("#startstop").html("stop");
-    document.getElementById("startstop").addEventListener("click", stop);
-    started = true;
-    loop();
-}
-
-function stop() {
-    select("#startstop").html("start");
-    document.getElementById("startstop").addEventListener("click", start);
-    removeblur();
-    started = false;
-    noLoop();
-}
-
-var rightEye,
-    leftEye,
-    leftear,
-    rightear,
-    leftshoulder,
-    rightshoulder,
-    defaultRightEarPosition = [],
-    defaultLeftEarPosition = [],
-    defaultRightshoulderPosition = [],
-    defaultLeftshoulderPosition = [],
-    defaultRightEyePosition = [],
-    defaultLeftEyePosition = [];
-
-
-function calEyes() {
-    for (let i = 0; i < poses.length; i++) {
-        let pose = poses[i].pose;
-        for (let j = 0; j < pose.keypoints.length; j++) {
-            let keypoint = pose.keypoints[j];
-            rightear = pose.keypoints[4].position;
-            rightshoulder = pose.keypoints[6].position;
-            rightEye = pose.keypoints[2].position;
-
-
-            while (defaultRightEyePosition.length < 1) {
-                defaultRightEyePosition.push(rightEye.y);
-            }
-
-            while (defaultRightEarPosition.length < 1) {
-                defaultRightEarPosition.push(rightear.y);
-            }
-
-            while (defaultRightshoulderPosition.length < 1) {
-                defaultRightshoulderPosition.push(rightshoulder.y);
-            }
-
-
-            RightEarPosition.push(rightear.y);
-            RightEyePosition.push(rightEye.y);
-            RightshoulderPosition.push(rightshoulder.y);
-
-            text(RightEarPosition, 100, 100);
-
-
-
-
-
-
-            if (Math.abs(rightEye.y - defaultRightEyePosition[0]) > 20) {
-                blur();
-            }
-            if (Math.abs(rightEye.y - defaultRightEyePosition[0]) <= 20) {
-                removeblur();
-            }
-        }
-    }
-}
-
-
-
-function blur() {
-    document.getElementById("audioElement").play();
-    document.body.style.filter = "blur(5px)";
-    document.body.style.transition = "1s";
-    console.log("Change");
-}
-
-function removeblur() {
-    document.body.style.filter = "blur(0px)";
-    var audio = document.getElementById("audioElement").pause();
 }
